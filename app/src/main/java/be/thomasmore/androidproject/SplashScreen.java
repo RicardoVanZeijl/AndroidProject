0377e3a3c993bd86df4dfaf7fb1cb5e6fdd30e17
larsg <larsgijbels@gmail.com>
2017-12-04 23:24:32 +0100

    test splashscreen still not work

--- app/src/main/java/be/thomasmore/androidproject/SplashScreen.java
+++ app/src/main/java/be/thomasmore/androidproject/SplashScreen.java
@@ -9,8 +9,9 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
 
-public class SplashScreen extends Activity {
+public class SplashScreen extends AppCompatActivity {
 
     // Splash screen timer
     private static int SPLASH_TIME_OUT = 3000;
