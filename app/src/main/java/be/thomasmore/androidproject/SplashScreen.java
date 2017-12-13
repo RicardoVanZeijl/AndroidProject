9b582837a4b61f1b0f7280156e98781096bd27fb
larsg <larsgijbels@gmail.com>
2017-12-13 20:39:55 +0100

    Splashscreen working

--- app/src/main/java/be/thomasmore/androidproject/SplashScreen.java
+++ app/src/main/java/be/thomasmore/androidproject/SplashScreen.java
@@ -4,41 +4,35 @@ package be.thomasmore.androidproject;
  * Created by larsg on 4/12/2017.
  */
 
-
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 
+import static java.lang.Thread.sleep;
+
 public class SplashScreen extends AppCompatActivity {
 
-    // Splash screen timer
-    private static int SPLASH_TIME_OUT = 3000;
+    private final int SPLASH_DISPLAY_LENGTH = 1000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setContentView(R.layout.activity_splash);
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.splash);
 
         new Handler().postDelayed(new Runnable() {
-
-            /*
-             * Showing splash screen with a timer. This will be useful when you
-             * want to show case your app logo / company
-             */
-
             @Override
             public void run() {
-                // This method will be executed once the timer is over
-                // Start your app main activity
-                Intent i = new Intent(SplashScreen.this, MainActivity.class);
-                startActivity(i);
 
-                // close this activity
-                finish();
+                // naar startscherm
+                Intent i;
+                i = new Intent(SplashScreen.this, Startpagina.class);
+                SplashScreen.this.startActivity(i);
+                SplashScreen.this.finish();
+
             }
-        }, SPLASH_TIME_OUT);
+        }, SPLASH_DISPLAY_LENGTH);
     }
-
 }
+
+
--- app/src/main/java/be/thomasmore/androidproject/Startpagina.java
+++ app/src/main/java/be/thomasmore/androidproject/Startpagina.java
@@ -5,11 +5,11 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class Startpagina extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
