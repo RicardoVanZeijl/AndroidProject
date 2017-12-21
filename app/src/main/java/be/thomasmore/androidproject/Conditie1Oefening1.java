20c622a586891129688226e896bdd26d62d65971
Ricardo van Zeijl <ricardo.vanzeijl@gmail.com>
2017-12-21 17:36:22 +0100

    Design Conditie 1 Oefeningen + Bijhorende drawables

--- app/src/main/java/be/thomasmore/androidproject/Conditie1Oefening1.java
+++ app/src/main/java/be/thomasmore/androidproject/Conditie1Oefening1.java
@@ -2,25 +2,57 @@ package be.thomasmore.androidproject;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Created by Ricardo van Zeijl.
+ *
+ */
+
 public class Conditie1Oefening1 extends AppCompatActivity {
 
+    TextToSpeech tts;
+
+    private DatabaseHelper db;
+
+    List<Woord> woorden;
+
     long studentID;
+    long onderdeelID = 1;
+
+    int index = 0;
+    int punten = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_conditie1_oefening1);
 
+        db = new DatabaseHelper(this);
 
+        Bundle bundle = getIntent().getExtras();
+        studentID = bundle.getLong("studentID");
+
+        Student student = db.getStudent(studentID);
+
+        Onderzoek onderzoek;
+        onderzoek = db.getOnderzoek(student.getGroepID(), onderdeelID);
+
+        long lijstID = onderzoek.getLijstID();
+
+        woorden = db.getWoordenFromLijst(lijstID);
+        Collections.shuffle(woorden);
     }
 
     public void onClickVolgendeOefening(View view) {
         Bundle bundle = new Bundle();
         bundle.putLong("studentID", studentID);
-        Intent intent = new Intent(this, Preteaching.class  );
+        Intent intent = new Intent(this, Conditie1Oefening3.class  );
         intent.putExtras(bundle);
     }
 }
--- app/src/main/java/be/thomasmore/androidproject/Conditie1Oefening6.java
+++ app/src/main/java/be/thomasmore/androidproject/Conditie1Oefening6.java
@@ -1,11 +1,12 @@
 package be.thomasmore.androidproject;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
+
+/**
+ * Created by Ricardo van Zeijl.
+ *
+ */
 
 public class Conditie1Oefening6 extends AppCompatActivity {
 
@@ -13,17 +14,6 @@ public class Conditie1Oefening6 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_conditie1_oefening6);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
 }
